b6df0f90e0e7669f2b6e2a00b187aee76c46fee5
Ajumal <ajumal.ebrahim@eworks.in>
2013-03-14 19:35:55 +0530

    Revert "copyied"
    This reverts commit 244f9cd0466bef19579e0046d20a0168c5f87603.

--- GoogleDataTestAju/AppDelegate.h
+++ GoogleDataTestAju/AppDelegate.h
@@ -4,8 +4,6 @@
 //
 //  Created by Eworks on 08/03/13.
 //  Copyright (c) 2013 Eworks. All rights reserved.
-////  Created by Eworks on 08/03/13.
-//  Copyright (c) 2013 Eworks. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
